d3b6ff7d0f7aa26dbace3a8d625e08b07025db76
holasoypablo <jubuitrago@unal.edu.co>
2023-06-14 18:18:02 -0500

    primer intento

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,50 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    cluster = []
+    cant = []
+    porc = []
+    princ = []
+    claves = ''
 
-    return df
+    with open('clusters_report.txt', 'r') as file:
+        lines = file.readlines()
+    lines = lines[4:]
+
+    for linea in lines:
+        linea = linea.strip()
+        linea = re.split('\s+', linea)
+        if(linea[0].isdigit()):
+            numericos = linea[:linea.index('%')]
+            cluster.append(numericos[0])
+            cant.append(numericos[1])
+            porc.append(numericos[2].replace(',','.'))
+            if len(claves) != 0:
+                princ.append(claves)
+            claves = ' '.join(linea[linea.index('%')+1:])
+            print("Claves es",claves)
+        else:
+            claves = claves + ' '.join(linea)
+    princ.append(claves)
+
+    data = {'cluster': cluster, 'cantidad_de_palabras_clave': cant, 'porcentaje_de_palabras_clave': porc, 'principales_palabras_clave':princ}
+    df = pd.DataFrame(data)
+            
+            
+    
+    print(cluster,"Su largo es",len(cluster))
+    print(cant,"Su largo es",len(cant))
+    print(porc,"Su largo es",len(porc))
+    print(princ,"Su largo es",len(princ))
+    print(df)
+        
+
+
+
+print('Hecho por Juan Pablo Buitrago Diaz CC 1000.206.552')
+ingest_data()
